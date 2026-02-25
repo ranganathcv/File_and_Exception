0a7fe26c96bf1f9d0164fa8c3ba00e9d40670dec
Ranganath <ranganathcv123@gmail.com>
2026-02-25 23:05:35 +0530

    delete factorial_task1.py

--- factorial_task1.py
+++
@@ -1,8 +0,0 @@
-def factorial(num):
-    if num ==1:
-        return 1
-    else:
-        return num * factorial(num-1)
-
-num=int(input("Enter a number :"))
-print(f'Factorial of {num} is : {factorial(num)}')
