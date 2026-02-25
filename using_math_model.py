f761648d8c62b4ef75121cf0999db9548498f1ba
Ranganath <ranganathcv123@gmail.com>
2026-02-25 23:07:48 +0530

    delete using_math_model.py

--- using_math_model.py
+++
@@ -1,8 +0,0 @@
-import math as mt
-number=int(input("Enter the number :"))
-squre_root=mt.sqrt(number)
-print(f'squre root of {number} is : {squre_root}')
-Logritham=mt.log(number)
-print(f'logeitham of {number} is : {Logritham}')
-sin_number=mt.sin(number)
-print(f'sin of {number} is  : {sin_number}')
